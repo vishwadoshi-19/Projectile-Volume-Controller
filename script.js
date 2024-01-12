e91218ef6090e19dc6e06cde87bb8cf80b215939
Vishwa Doshi <vishwadoshi19@outlook.com>
2024-01-12 16:53:13 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-var line = document.querySelector('hr');
 
-var maxVolume = line.offsetWidth;
+
+let line = document.querySelector('hr');
+
+let maxVolume = line.offsetWidth;
+
+// for default value at 0
+const dot = document.querySelector('.circle')
+const rand = Math.floor((Math.random()*(maxVolume+1)))
+dot.style.transform = 'translateX(' + 0 + 'px)';
 
 console.log("Max Volume:", maxVolume);
+
+function moveDot() {
+    const dot = document.querySelector('.circle')
+    const rand = Math.floor((Math.random()*(maxVolume+1)))
+    dot.style.transform = 'translateX(' + rand + 'px)';
+
+    let audio = new Audio('sound2.mp3');
+    audio.play();
+
+    let volume = document.querySelector('.percent');
+    volume.textContent = Math.round((rand/maxVolume)*100) + '%';
+}
