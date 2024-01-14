4a52abdb27e769df3595364b005670e6ae7cfafe
Vishwa Doshi <vishwadoshi19@outlook.com>
2024-01-14 19:02:38 +0530

    second commit , audio added and rotation arc started lol

--- script.js
+++ script.js
@@ -22,3 +22,45 @@ function moveDot() {
     let volume = document.querySelector('.percent');
     volume.textContent = Math.round((rand/maxVolume)*100) + '%';
 }
+
+var isMouseDown = false;
+var initialAngle = 0;
+
+var rotatableElement = document.querySelector('.icon');
+
+rotatableElement.addEventListener('mousedown', function(event) {
+  isMouseDown = true;
+  initialAngle = getRotationAngle(event);
+//   var currentAngle = getRotationAngle(event);
+    var rotation = -10;
+
+    // Apply rotation to the element
+    rotatableElement.style.transform = 'rotate(' + rotation + 'deg)';
+});
+
+document.addEventListener('mouseup', function() {
+  isMouseDown = false;
+});
+
+// document.addEventListener('mousemove', function(event) {
+//   if (isMouseDown) {
+//     var currentAngle = getRotationAngle(event);
+//     var rotation = currentAngle - initialAngle;
+
+//     // Apply rotation to the element
+//     rotatableElement.style.transform = 'rotate(' + rotation + 'deg)';
+//   }
+// });
+
+function getRotationAngle(event) {
+  var rect = rotatableElement.getBoundingClientRect();
+  var center = {
+    x: rect.left + rect.width / 2,
+    y: rect.top + rect.height / 2
+  };
+
+  var angle = Math.atan2(event.clientY - center.y, event.clientX - center.x);
+  angle = angle * (180 / Math.PI);
+
+  return angle;
+}
